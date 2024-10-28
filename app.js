9fec81f11eba266ccb720cecd0888c962cbe673c
johnathanponsoldt <jponsoldt772@gmail.com>
2024-10-27 23:42:24 -0400

    Implement shape drawing logic for lines, rectangles, and circles.

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const canvas = document.getElementById('myCanvas');
 
 // Get the 2D drawing context
 const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#4287f5';  // Set the fill color
+ctx.fillRect(0, 0, canvas.width, canvas.height);  // Draw a filled rectangle covering the entire canvas
+
+ctx.fillStyle = '#45b500';
+ctx.fillRect(50, 50, 150, 100);
+
+// Rectangle outline
+ctx.strokeStyle = '#b50030';
+ctx.lineWidth = 5;
+ctx.strokeRect(250, 50, 150, 100);
